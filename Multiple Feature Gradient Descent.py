f7b0a7088555c63dc42ad27f41821012dd3b78a8
Kinshuk Phalke <69013394+xinslu@users.noreply.github.com>
2021-05-20 20:12:03 +0400

    Multi Feature Gradient Descent

--- Multiple Feature Gradient Descent.py
+++ Multiple Feature Gradient Descent.py
@@ -3,13 +3,16 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def graph_prediction(X,theta,data):
 	x = np.linspace(X.min(),X.max(), 1000)
-	plt.plot(x, theta[0]+x*theta[1], '.r')
+	plt.plot(x, theta[0]+x*theta[1], ':r')
 	lineX=np.linspace(X.min(), X.max(), 100)
-	plt.xlim([5,25])
-	plt.plot(data[:,[1]],data[:,[2]],'rx')
+	plt.plot(data[:,[1]],data[:,[3]],'rx')
+	plt.xlabel("Size")
+	plt.ylabel("Price")
+	plt.title('Size vs. Price')
+	plt.legend(['Prediction','Data'])
+	plt.xlim([-1,4])
 	plt.grid(True)
 	plt.show()
 
@@ -20,21 +23,22 @@ def graph_cost(iterations,cost):
 	plt.title('Error vs. Training Epoch')
 	plt.show()
 
-data = pd.read_csv("./input/ex1data1.txt" , header=None, names=['Size', 'Profit'])
+data = pd.read_csv("./input/ex1data2.txt" , header=None, names=['Size', 'Bedrooms', 'Price'])
+data = (data - data.mean()) / data.std()
 data.insert(0, 'Ones', 1)
 cols = data.shape[1]
-m= data.shape[0]
+m=data.shape[0]
 X = np.array(data.iloc[:,0:cols-1])
 Y = np.array(data.iloc[:,cols-1:cols])
-theta = np.zeros((2, 1))
 data=np.array(data)
+theta = np.zeros((3, 1))
 iterations=1500
 alpha = 0.01
 hypothesis=X.dot(theta)
 cost = np.zeros(iterations)
 for i in range(iterations):
-	prediction=X.dot(theta)
-	theta=theta-(X.T.dot((prediction-Y))/m)*alpha
-	cost[i]=np.sum((prediction-Y)**2)/2/m
+ 	prediction=X.dot(theta)
+ 	theta=theta-(X.T.dot((prediction-Y))/m)*alpha
+ 	cost[i]=np.sum((prediction-Y)**2)/2/m
 
 graph_cost(iterations,cost)
