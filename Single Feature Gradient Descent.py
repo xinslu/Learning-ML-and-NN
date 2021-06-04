dacbd24d2775be8174913bed89ed841622ee157d
Kinshuk Phalke <phalkekinshuk@gmail.com>
2021-06-04 16:36:48 +0400

    Added Gaussian Kernel

--- Single Feature Gradient Descent.py
+++ Single Feature Gradient Descent.py
@@ -3,28 +3,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def graph_prediction(X,theta,data):
-	x = np.linspace(X.min(),X.max(), 1000)
-	plt.plot(x, theta[0]+x*theta[1], '.r')
-	lineX=np.linspace(X.min(), X.max(), 100)
-	plt.xlim([5,25])
-	plt.plot(data[:,[1]],data[:,[2]],'rx')
-	plt.grid(True)
-	plt.show()
+def graph_prediction(X, theta, data):
+    x = np.linspace(X.min(), X.max(), 1000)
+    plt.plot(x, theta[0] + x * theta[1], '.r')
+    lineX = np.linspace(X.min(), X.max(), 100)
+    plt.xlim([5, 25])
+    plt.plot(data[:, [1]], data[:, [2]], 'rx')
+    plt.grid(True)
+    plt.show()
 
-def graph_cost(iterations,cost):
-	plt.plot(np.arange(iterations),cost,'r.')
-	plt.xlabel('Iterations')
-	plt.ylabel('Cost')
-	plt.title('Error vs. Training Epoch')
-	plt.show()
 
-data = pd.read_csv("./input/ex1data1.txt" , header=None, names=['Size', 'Profit'])
+def graph_cost(iterations, cost):
+    plt.plot(np.arange(iterations), cost, 'r.')
+    plt.xlabel('Iterations')
+    plt.ylabel('Cost')
+    plt.title('Error vs. Training Epoch')
+    plt.show()
+
+
+data = pd.read_csv("./input/ex1data1.txt", header=None,
+                   names=['Size', 'Profit'])
 data.insert(0, 'Ones', 1)
 cols = data.shape[1]
 m = data.shape[0]
-X = np.array(data.iloc[:,0:cols-1])
-Y = np.array(data.iloc[:,cols-1:cols])
+X = np.array(data.iloc[:, 0:cols - 1])
+Y = np.array(data.iloc[:, cols - 1:cols])
 theta = np.zeros((2, 1))
 data = np.array(data)
 iterations = 1500
@@ -32,8 +35,8 @@ alpha = 0.01
 hypothesis = X.dot(theta)
 cost = np.zeros(iterations)
 for i in range(iterations):
-	prediction = X.dot(theta)
-	theta=theta-(X.T.dot((prediction-Y))/m)*alpha
-	cost[i]=np.sum((prediction-Y)**2)/2/m
+    prediction = X.dot(theta)
+    theta = theta - (X.T.dot((prediction - Y)) / m) * alpha
+    cost[i] = np.sum((prediction - Y)**2) / 2 / m
 
-graph_cost(iterations,cost)
+graph_cost(iterations, cost)
---
+++ Support Vector Machine-Gaussian Kernel.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io
+from sklearn.svm import SVC
+from matplotlib.colors import ListedColormap
+
+
+def plot_decision_regions(X, y, classifier, resolution=0.01):
+    plot_data(X, y)
+    x1_min = X[:, 0].min() - 1
+    x1_max = X[:, 0].max() + 1
+    x2_min = X[:, 1].min() - 1
+    x2_max = X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    cs = plt.contour(xx1, xx2, Z, levels=[0.15])
+    plt.xlim(0, 1)
+    plt.ylim(0, 1)
+    plt.show()
+
+
+def plot_data(X, y):
+    Accepted = X[np.array(np.where(y == 1))[0]]
+    Failed = X[np.array(np.where(y == 0))[0]]
+    plt.plot(Accepted[:, [0]], Accepted[:, [1]], 'k+')
+    plt.plot(Failed[:, [0]], Failed[:, [1]], 'y.')
+
+
+def split_data(data, totalData):
+    trainIndex, testIndex = list(), list()
+    for i in range(1, totalData):
+        if np.random.uniform(0, 1) < 0.7:
+            trainIndex += [i]
+        else:
+            testIndex += [i]
+    trainData = data[trainIndex]
+    testData = data[testIndex]
+    return trainData, testData
+
+
+data = scipy.io.loadmat("./input/ex6data2.mat")
+X = data['X']
+y = data['y']
+data = np.hstack((X, y))
+totalData = X.shape[0]
+[trainData, testData] = split_data(data, totalData)
+svclassifier = SVC(kernel='rbf', C=1, gamma=50)
+svclassifier.fit(trainData[:, 0:2], trainData[:, [2]].ravel())
+plot_decision_regions(X, y, svclassifier)
