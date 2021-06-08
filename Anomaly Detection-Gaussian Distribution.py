ca423fc7a40007af441ab54d6c48617c12714639
Kinshuk Phalke <phalkekinshuk@gmail.com>
2021-06-08 11:59:45 +0400

    Added Anomaly Detection using Gaussian Distribution

--- Anomaly Detection-Gaussian Distribution.py
+++ Anomaly Detection-Gaussian Distribution.py
@@ -23,7 +23,7 @@ def plotDecisionBoundary(X, mu, sigma2):
                         np.ravel(V).reshape(5041, 1)], axis=1)
     Z = multiVariateGaussian(X, mu, sigma2)
     Z = Z.reshape(71, 71)
-    plt.contour(U, V, Z, levels=[10**i for i in range(-20, -2, 3)])
+    plt.contour(U, V, Z, levels=[10**i for i in range(-20, -1, 3)])
     plt.show()
 
 
@@ -35,8 +35,34 @@ def multiVariateGaussian(X, mu, sigma2):
     return p
 
 
+def selectThreshold(yval, pval):
+    bestEpsilon = 0
+    bestF1 = 0
+    F1 = 0
+    stepsize = (np.max(pval) - np.min(pval)) / 1000
+    for epsilon in np.arange(np.min(pval), np.max(pval), stepsize):
+        cvPredictions = (pval < epsilon)
+        fp = np.sum(np.logical_and(cvPredictions == 1, yval == 0))
+        tp = np.sum((np.logical_and(cvPredictions == 1, yval == 1)))
+        fn = np.sum(np.logical_and(cvPredictions == 0, yval == 1))
+        prec = tp / (tp + fp)
+        rec = tp / (tp + fn)
+        F1 = 2 * prec * rec / (prec + rec)
+        if F1 > bestF1:
+            bestF1 = F1
+            bestEpsilon = epsilon
+    return bestEpsilon, bestF1
+
+
 data = loadmat("./input/ex8data1.mat")
 X = data['X']
+Xval = data['Xval']
+yval = data['yval']
 [mu, sigma2] = estimateGaussian(X)
 p = multiVariateGaussian(X, mu, sigma2)
+pval = multiVariateGaussian(Xval, mu, sigma2)
+pval = np.reshape(pval, (pval.shape[0], 1))
+[epsilon, F1] = selectThreshold(yval, pval)
+outliers = np.where(p < epsilon)
+plt.plot(X[outliers][:, [0]], X[outliers][:, [1]], 'ro', markersize=12)
 plotDecisionBoundary(X, mu, sigma2)
